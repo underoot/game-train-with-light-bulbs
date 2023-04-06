9ade9b3c2976d6f6c2062ed7941ba6c9e55b585f
Aleksandr Shoronov <alexshoronov@gmail.com>
2023-04-07 00:30:17 +0300

    Updates before publish

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
+/** @type {import('vite').UserConfig} */
 module.exports = {
-  base: '/game-train-with-light-bulbs/'
+  base: '/game-train-with-light-bulbs/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js'
+      }
+    }
+  }
 };
